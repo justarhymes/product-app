8eafddb20529b0770846fae33726da91d07a3c3d
Justin <justin@dapshow.com>
2016-01-07 04:03:10 -0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name productApp
+ * @description
+ * # productApp
+ *
+ * Main module of the application.
+*/
+angular.module('productApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'ProductController',
+        controllerAs: 'shop'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ProductApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ProductApp
+*/
+var app = angular.module('productApp');
+
+app.controller('AboutCtrl', function () {
+  this.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
+});
